a1fc47777e11c4de3a2f8d118f6a38c50984926e
NSP13737 <63027021+NSP13737@users.noreply.github.com>
2025-12-19 17:25:10 -0600

    fixed (part of) syncing function for belt

--- include/udp_utils.h
+++ include/udp_utils.h
@@ -7,7 +7,7 @@
 void udpSetupWireless(const char *ssid, const char *password, const int UdpPort);
 
 /**
- * @brief Using quatratic function: Takes the users raw distance from a wall and converts it to a percentage to be used by modulation helpers
+ * @brief Gets new data from UDP connection, if there is any.
  * @param current_data Contains 16 floats. The first 8 represent raw tactors distances, the next 8 represent study parameters.
  * @return If there is a new packet to recieve, returns floats for this, otherwise, returns values originally passed in.
  */
